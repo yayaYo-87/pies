85d7c4949964edb068834ae2a2015ac4a300e028
yayayo <g.voytko@yandex.ru>
2017-12-09 18:31:22 +0300

    orders:  create cookie

--- app/market/views.py
+++ app/market/views.py
@@ -1,5 +1,22 @@
 from django.views import generic
 
+from app.orders.models import Cart, OrderGoods
+
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
+
+    def get(self, *args, **kwargs):
+        data = super(IndexView, self).get(*args, **kwargs)
+        if self.request.session.is_empty():
+            self.request.session.create()
+        if not Cart.objects.filter(cookie=self.request.session.session_key):
+            cart = Cart.objects.create(
+                cookie=self.request.session.session_key
+            )
+        else:
+            cart = Cart.objects.filter(cookie=self.request.session.session_key).first()
+        if self.request.session.session_key:
+            OrderGoods.objects.filter(
+                cart=cart, active=False).delete()
+        return data
